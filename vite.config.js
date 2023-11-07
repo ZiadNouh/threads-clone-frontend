91f7a7e86702aa574a70a4317a1c86ad755689f8
ziad <ziad.nouh111@gmail.com>
2023-11-08 01:47:54 +0200

    a

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    // Get rid of the CORS error
-    proxy: {
-      "/api": {
-        target: "https://threads-clone-backend-fqmz.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+
   build: {
     outDir: "build", // Specify the desired output directory
   },
