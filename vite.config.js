36678955158bb45fe104069c066c22b3cf1515e7
ZiadNouh <64560345+ZiadNouh@users.noreply.github.com>
2023-11-07 17:46:15 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-
     // Get rid of the CORS error
     proxy: {
       "/api": {
@@ -15,5 +13,8 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  build: {
+    outDir: "build", // Specify the desired output directory
   },
 });
